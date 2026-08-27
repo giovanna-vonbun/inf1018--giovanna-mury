5bdf29ceb68be99ce5fd721082eb16a8662b8e8c
Giovanna Mury Vonbun <giomury@gmail.com>
2026-08-27 23:41:07 +0000

    ex3

--- lab_3/ex3.c
+++ lab_3/ex3.c
@@ -1,12 +1,24 @@
+#include <stdio.h>
+
+
 unsigned char switch_byte(unsigned char x){
-    unsigned char b = (a & 0x0F) << 4; // mascara + isolar B
-    unsigned char c = a >> 4; // isolar a
+    unsigned char b = (x & 0x0F) << 4; // mascara + isolar B
+    unsigned char c = x >> 4; // isolar a
     
     return b | c; // junta os dois
 }
-unsigned char rotate_left(unsigned char x, int n);
+unsigned char rotate_left(unsigned char x, int n){
+
+    unsigned char b = (x & 0x0F) << n; 
+    unsigned char c = x >> 8-n;
+
+    return b | c; 
+
+}
 
 int main (void){
-    
+    unsigned char oldx = 0xAB;
+    unsigned char novox = switch_byte(oldx);
+    printf("Ox%02X -> 0x%02X\n", oldx, novox);
     return 0;
 }
